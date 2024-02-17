7c7f93c0d0080a9511725f4ce4e97b3063ca4915
Rick Miller <rick@pulpfreepress.com>
2024-02-17 12:04:39 -0500

    Initial Commit\!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# c_bootcamp
+![C Programming Boot Camp](images/C_Bootcamp_Header.jpg)
+
+# C Programming Boot Camp
 Code examples and resources for C Programming Boot Camp
---
+++ hello_world/hello_world.c
@@ -0,0 +1,14 @@
+/************************************************
+ * Project: hello_world.c
+ * 
+ * Everyone's first C program.
+ * 
+************************************************/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello, world!\n");
+    puts("Hello, World!");
+    return 0;
+}
