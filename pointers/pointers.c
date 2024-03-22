e9491d1c752d8d5e6253633b40e1ff2ef036c281
Rick Miller <rick@pulpfreepress.com>
2024-03-22 15:42:57 -0400

    Worked Pointers Example.

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -4,15 +4,12 @@
 
 int main(){
     int a = 3;
-    int* pa = &a;
-
+    int* a_ptr = &a;
     printf("a = %d\n", a);
     printf("address of a = %p\n", &a);
-
-    printf("a = %d\n", *pa);
-    printf("address of a = %p\n", pa);
-
-
-
+    printf("a_ptr = %p\n", a_ptr);
+    printf("address of a_ptr = %p\n", &a_ptr);
+    printf("a = %d\n", *a_ptr);
+    printf("address of a = %p\n", a_ptr);
     return 0;
 }
