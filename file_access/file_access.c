a58318d1c7c1f25227f350c10b5dd36de038a529
Rick Miller <rick@pulpfreepress.com>
2024-03-23 16:02:30 -0400

    Worked file_access example.

--- file_access/file_access.c
+++ file_access/file_access.c
@@ -4,18 +4,27 @@
 
 int main(){
 
-    FILE *names_file = NULL;
-    char line[200];
-    size_t size = 200;
-    size_t characters;
+    FILE *fp = NULL;
+    char *input_string = NULL;
+    size_t size = 0;
+    size_t characters_read;
+    short keep_going = 1;
 
-    names_file = fopen("names.txt", "w");
-    while((characters = getline(&line, &size, stdin)) != EOF){
-        if(names_file){
-            fputs(characters, names_file);
+    fp = fopen("lines.txt", "a");
+
+    while(keep_going){
+        printf("Enter a line of text: ");
+        characters_read = getline(&input_string, &size, stdin);
+        printf("%lu\n", characters_read);
+        if(characters_read > 1 ){
+            if(fp){
+                fputs(input_string, fp);
+            }
+        } else {
+            keep_going = 0;
+            fclose(fp);
         }
     }
-
 
     return 0;
 }
