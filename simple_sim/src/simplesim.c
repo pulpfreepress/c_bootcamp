372254782461cdbfdce7e6f44435b4483be5725c
Rick Miller <rick@pulpfreepress.com>
2024-09-27 17:56:07 -0400

    Worked on simplesim.

--- simple_sim/src/simplesim.c
+++ simple_sim/src/simplesim.c
@@ -2,7 +2,134 @@
 
 #include "simplesim.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+
+CPU cpu;
+
+void initialize_cpu(CPU * cpu){
+    cpu->accumulator = 0;
+    cpu->program_counter = 0;
+    for(int i = 0; i < MAX_MEMORY; i++){
+        cpu->memory[i] = 0;
+    }
+}
+
+
+void load_program(CPU * cpu, char * filename){
+
+
+}
+
+void dump_cpu_state(CPU * cpu){
+    printf("********* CPU STATE *********\n");
+    printf("Accumulator:     %0.4f\n", cpu->accumulator);
+    printf("Program Counter: %d\n", cpu->program_counter);
+    printf("********* MEMORY *********\n");
+    for(int i = 1; i <= MAX_MEMORY; i++){
+        if((i % 10) == 0){
+            printf("%04.0f\n", cpu->memory[i-1]);
+        } else {
+            printf("%04.0f ", cpu->memory[i-1]);
+        }
+    }
+}
+
+void load_demo(CPU * cpu){
+    cpu->memory[0] = 1010;
+    cpu->memory[1] = 1011;
+    cpu->memory[2] = 2010;
+    cpu->memory[3] = 3011;
+    cpu->memory[4] = 2112;
+    cpu->memory[5] = 1112;
+    cpu->memory[6] = 4020;
+    cpu->memory[20] = 4300;
+}
 
 void run(){
-    printf("Simple Sim Running...");
+    initialize_cpu(&cpu);
+    load_demo(&cpu);
+    dump_cpu_state(&cpu);
+
+    int keepgoing = TRUE;
+
+    while(keepgoing){
+        int instruction = (int)cpu.memory[cpu.program_counter++];
+
+        int opcode = instruction / 100;
+        int operand = instruction % 100;
+
+        switch(opcode){
+            case READ: {
+                printf("Please enter a number: ");
+                double input = 0;
+                scanf("%lf", &input);
+                cpu.memory[operand] = input;
+                break;
+            }
+            
+            case WRITE: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case LOAD: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case STORE: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case ADD: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case SUB: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case MUL: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case DIV: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case JMP: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case JMPZ: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case JMPN: {
+                printf("Not Implemented...\n");
+                break;
+            }
+
+            case HALT:{
+                keepgoing = FALSE;
+                break;
+            }
+            default:{
+                keepgoing = FALSE;
+            }
+        }
+    } // end while
+
+    dump_cpu_state(&cpu);
+
+
+
 }
--- simple_sim/src/simplesim.h
+++ simple_sim/src/simplesim.h
@@ -4,7 +4,46 @@
 #define SIMPLESIM_H
 
 
+#include <stdio.h>
+
+// Memory
+#define MAX_MEMORY 100
+
+// OPCODES in Decimal
+#define READ 10     // Read number from console into memory location
+#define WRITE 11    // Write number from memory location to console
+#define LOAD 20     // Load value located at memory location into accumulator
+#define STORE 21    // Store value in accumulator to memory location
+#define ADD 30      // Add value from specified memory location to accumulator. Leave results in accumulator.
+#define SUB 31      // Subtract value found in memory location from accumulator. Leave results in accumulator.
+#define MUL 32      // Multiply accumulator by value found at memory location. Leave results in accumulator.
+#define DIV 33      // Divide accumulator by value at memory location. Leave results in accumulator.
+#define JMP 40      // Absolute jump to memory location indicated by PC.
+#define JMPN 41     // Jump to memory location indicated by PC if accumulator value is negative.
+#define JMPZ 42     // Jump to memory location indicated by PC if accumulator value is zero.
+#define HALT 43     // Stop program execution.
+
+#define TRUE 1
+#define FALSE 0
+
+struct cpu {
+    int program_counter;
+    double accumulator;
+    double memory[MAX_MEMORY];
+};
+
+typedef struct cpu CPU;
+
+
+// Function Prototypes
 void run();
+void initialize_cpu(CPU * cpu);
+void load_program(CPU * cpu, char * filename);
+void load_demo(CPU * cpu);
+void dump_cpu_state(CPU * cpu);
+
+
+
 
 
 #endif // simplesim.h
