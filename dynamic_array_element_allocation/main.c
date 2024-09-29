bc429a9bbc6dd4b38e9baf24d6dd9fa9aa11233d
swodog <rick@pulpfreepress.com>
2024-09-29 07:43:47 -0400

    Worked Dynamic Array Element Allocation

--- dynamic_array_element_allocation/main.c
+++ dynamic_array_element_allocation/main.c
@@ -20,59 +20,51 @@ typedef struct person Person;
 /* Declare function prototype. */
 void print_people_array(Person **array, int size);
 
-
 int main(){
-
-	// Declare pointer to Person pointer.
 	Person **people_array = NULL;
 
-	// Dynamically allocate memory for the array.
 	people_array = malloc(sizeof(Person *) * ARRAY_LENGTH);
 
-	// If the memory allocation for people_array succeeded...
 	if(people_array){
 
-		// Sets each array element to NULL
-		for(int i = 0; i < (sizeof(people_array)/sizeof(Person *)); i++){
+		for(int i = 0; i < ARRAY_LENGTH; i++){
 			people_array[i] = NULL;
 		}
-
-		// Allocate a structure and assign to first element.
+	
 		people_array[0] = malloc(sizeof(Person));
 
-		// If the Person structure memory allocatioin is successful...
 		if(people_array[0]){
 			strcpy(people_array[0]->first_name, "Coralie");
 			strcpy(people_array[0]->middle_name, "Sarah");
 			strcpy(people_array[0]->last_name, "Miller");
-
-			printf("First Name: %s\n", people_array[0]->first_name);
-			printf("Middle Name: %s\n", people_array[0]->middle_name);
-			printf("Last Name: %s\n", people_array[0]->last_name);
-
-
-		} else {
-			printf("Memory allocation for Person object failed.\n");
 		}
 
-		printf("********* Calling print_people_array() function...\n");
+		printf("First Name: %s\n", people_array[0]->first_name);
+		printf("Middle Name: %s\n", people_array[0]->middle_name);
+		printf("Last Name: %s\n", people_array[0]->last_name);
 
-		print_people_array(people_array, (sizeof(people_array)/sizeof(Person *)));
+		for(int i = 1; i < ARRAY_LENGTH; i++){
+			people_array[i] = malloc(sizeof(Person));
+			if(people_array[i]){
+				strcpy(people_array[i]->first_name, "Jane");
+				strcpy(people_array[i]->middle_name, "J.");
+				strcpy(people_array[i]->last_name, "Doe");
+			}
+			
+		}
 
-	
-		// First, free up memory for each instance if not NULL
-		for(int i = 0; i < (sizeof(people_array)/sizeof(Person *)); i++ ){
+		print_people_array(people_array, ARRAY_LENGTH);
+
+		for(int i = 0; i < ARRAY_LENGTH; i++){
 			if(people_array[i]){
 				free(people_array[i]);
 			}
 		}
 
-		// Free memory for the array itself.
 		free(people_array);
 
 	} else {
-
-		printf("Memory allocation for people_array failed.\n");
+		printf("Memory allocation failed for people_array.");
 	}
 
 	return 0;
@@ -82,8 +74,10 @@ void print_people_array(Person **array, int size){
 	for(int i = 0; i < size; i++){
 		if(array[i]){
 			printf("Person %d: %s\t%s\t%s\n", i, 
-					array[i]->first_name, array[i]->middle_name, array[i]->last_name);
+				array[i]->first_name, array[i]->middle_name, array[i]->last_name);
 		}
 	}
 
 }
+
+
