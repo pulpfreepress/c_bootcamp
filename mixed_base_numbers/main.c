0b19bd7541fddb134cc9f643ddd6de61ff2027bb
Rick Miller <rick@pulpfreepress.com>
2025-02-27 20:25:34 -0500

    Worked mixed-base example

--- mixed_base_numbers/main.c
+++ mixed_base_numbers/main.c
@@ -5,9 +5,9 @@ int main()
     unsigned char a=0x64;
     int b=0xFAFA;
 	
-    printf("value of a: %X [%x]\n",a,a);
-    printf("value of b: %X [%x]\n",b,b);
-    printf("value of b: %X [%X]\n",b,b);
+    printf("value of a: %X [%x]\n", a, a);
+    printf("value of b: %X [%x]\n", b, b);
+    printf("value of b: %X [%X]\n", b, b);
 	
     return 0;
 }
